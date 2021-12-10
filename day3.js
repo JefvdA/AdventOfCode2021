cc4469acb43a4168c8c908e42376e8294c1a1510
JefvdA <jef.v.d.a@live.be>
2021-12-10 15:37:50 +0100

    Day3 - part2 progress

--- day3.js
+++ day3.js
@@ -1,11 +1,18 @@
 import fs from 'fs'
 
-var array = fs.readFileSync('./input/day3.txt').toString().split("\n")
+var array = fs.readFileSync('./input/testing.txt').toString().split("\n")
 
 var gamma = findMostCommonBits(array)
 var epsilon = reverseBits(gamma)
 
-console.log(parseInt(gamma, 2) * parseInt(epsilon, 2))
+// var oxygenRating = selectByBitCriteria(array, "Oxygen")
+var co2Rating = selectByBitCriteria(array, "CO2")
+
+console.log("Power consumption: " + parseInt(gamma, 2) * parseInt(epsilon, 2))
+// console.log("Lifesupport rating: " + parseInt(oxygenRating, 2) * parseInt(co2Rating, 2))
+
+// console.log("OXYGEN: " + oxygenRating)
+console.log("CO2: " + co2Rating)
 
 function findMostCommonBits(array){
     var mostCommonBits = ""
@@ -15,6 +22,44 @@ function findMostCommonBits(array){
     }
 
     return mostCommonBits
+}
+
+function selectByBitCriteria(array, type){
+    var newArray = array
+    
+    for(var i in newArray[0].trim()){
+        var mostCommonBit = findMostCommonBit(newArray, i)
+        if(mostCommonBit == 2)
+            mostCommonBit = 1
+
+        for(var j in newArray){
+            switch(type){
+                case 'Oxygen':
+                    // newArray = newArray.filter((value, i) => {
+                    //     return value[i] == mostCommonBit
+                    // })
+                    if(newArray[j][i] != mostCommonBit)
+                        newArray.splice(j, 1)
+                    break
+                case 'CO2':
+                    // newArray = newArray.filter((value, i) => {
+                    //     return value[i] == leastCommonBit
+                    // })
+                    if(newArray[j][i] == mostCommonBit)
+                        newArray.splice(j, 1)
+                    break
+                default:
+                    return
+            }
+        }
+
+        console.log(newArray)
+
+        if(newArray.length <= 1)
+            break
+    }
+
+    return newArray[0]
 }
 
 function findMostCommonBit(array, i){
@@ -33,8 +78,10 @@ function findMostCommonBit(array, i){
 
     if(zeroCount > oneCount)
         return 0
-    else
+    else if(zeroCount < oneCount)
         return 1
+    else
+        return 2
 }
 
 function reverseBits(number){
