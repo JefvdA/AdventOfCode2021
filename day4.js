8b6d36336b48152b19cc74f833ef8f40a24ec3bf
Jef <jef.v.d.a@live.be>
2021-12-12 21:56:46 +0100

    Day4 - part1 more progress

--- day4.js
+++ day4.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 
-var array = fs.readFileSync('./input/testing.txt').toString().split("\n")
+var array = fs.readFileSync('./input/day4.txt').toString().split("\n")
 
 // Find chosen numbers
 var numbersString = array[0].trim()
@@ -26,6 +26,59 @@ for(var i = 2; i < array.length; i+=6){
 }
 
 // Play bingo
-numbers.forEach((number) => {
-    console.log(number)
-})
+for(var i in numbers){
+    var chosenNumber = numbers[i]
+    var winningNumber = -1
+    var winnerExists = false
+
+    for(var boardIndex in boards){
+        var board = boards[boardIndex]
+        
+        for(var rowIndex in board){
+            var row = board[rowIndex]
+            var rowLength = row.length
+
+            for(var numberIndex in row){
+                var number = row[numberIndex]
+                
+                if(number == chosenNumber){
+                    row.splice(numberIndex, 1)
+                    break
+                }
+            }
+
+            if(row.length == 0){
+                winnerExists = true
+                winningNumber = number
+                break
+            }
+            
+            if(rowLength > row.length)
+                break
+        }
+
+        if(winnerExists) {
+            var sumUnmarked = 0
+            var score = 0
+    
+            for(var rowIndex in board){
+                var row = board[rowIndex]
+
+                for(var numberIndex in row){
+                    var number = row[numberIndex]
+
+                    sumUnmarked += parseInt(number)                    
+                }
+            }
+
+            score = sumUnmarked * parseInt(chosenNumber)
+
+            console.log("Board nr " + boardIndex + " has won, last number was " + winningNumber)
+            console.log("That with a score of " + score)
+            break
+        }
+    }
+
+    if(winnerExists)
+        break
+}
