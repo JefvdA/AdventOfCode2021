8499db54f7e06edf75097c6f6e2179cc08aab05f
JefvdA <jef.v.d.a@live.be>
2021-12-13 17:21:52 +0100

    Day4 completed

--- day4.js
+++ day4.js
@@ -2,9 +2,6 @@ import fs from 'fs'
 
 var array = fs.readFileSync('./input/day4.txt').toString().split("\n")
 
-// Constants
-const markedNumber = -1
-
 // Find chosen numbers
 var numbersString = array[0].trim()
 var numbers = numbersString.split(',')
@@ -27,64 +24,105 @@ for(var i = 2; i < array.length; i+=6){
     boards.push(rows)
 }
 
-// Play bingo
-for(var i in numbers){
-    var chosenNumber = numbers[i]
-    var winningNumber = -1
-    var winnerExists = false
+findWinner()
+findLoser()
 
-    for(var boardIndex in boards){
-        var board = boards[boardIndex]
-        
-        for(var rowIndex in board){
-            var row = board[rowIndex]
-            var rowLength = row.length
+// Play bingo - PART 1 (find winner)
+function findWinner(){
+    for(var i in numbers){
+        var chosenNumber = numbers[i]
+        var winningNumber = -1
+        var winnerExists = false
 
-            for(var numberIndex in row){
-                var number = row[numberIndex]
-                
-                if(number == chosenNumber){
-                    row[numberIndex] = markedNumber
-                    break
-                }
-            }
-
-            if(VerifyWinner(board)){
-                winnerExists = true
-                winningNumber = number
-                break
-            }
+        for(var boardIndex in boards){
+            var board = boards[boardIndex]
             
-            if(rowLength > row.length)
-                break
-        }
-
-        if(winnerExists) {
-            var sumUnmarked = 0
-            var score = 0
-    
             for(var rowIndex in board){
                 var row = board[rowIndex]
 
                 for(var numberIndex in row){
                     var number = row[numberIndex]
+                    
+                    if(number == chosenNumber){
+                        row[numberIndex] *= -1
+                        break
+                    }
+                }
 
-                    if(number != markedNumber)
-                        sumUnmarked += parseInt(number)                    
+                if(VerifyWinner(board)){
+                    winnerExists = true
+                    winningNumber = number
+                    break
                 }
             }
 
-            score = sumUnmarked * parseInt(winningNumber)
+            if(winnerExists) {
+                var score = calculateScore(board, winningNumber)
+                console.log("Board nr " + boardIndex + " has won, last number was " + winningNumber)
+                console.log("That with a score of " + score)
+                break
+            }
+        }
 
-            console.log("Board nr " + boardIndex + " has won, last number was " + winningNumber)
+        if(winnerExists)
+            break
+    }
+}
+
+// Play bingo - PART 2 (find loser)
+function findLoser(){
+    for(var i in numbers){
+        var chosenNumber = numbers[i]
+        var loserExists = false
+        var winningNumber = -1
+
+        for(var boardIndex in boards){
+            var board = boards[boardIndex]
+            
+            for(var rowIndex in board){
+                var row = board[rowIndex]
+
+                for(var numberIndex in row){
+                    var number = row[numberIndex]
+                    
+                    if(number == chosenNumber){
+                        row[numberIndex] *= -1
+                        break
+                    }
+                }
+
+                if(VerifyWinner(board)){
+                    winningNumber = number
+                    var winnerCount = CountWinningBoards(boards)
+                    if(winnerCount == boards.length)
+                        loserExists = true
+                    break
+                }
+            }
+
+            if(loserExists)
+                break
+        }
+
+        if(loserExists){
+            var score = calculateScore(board, winningNumber)
+            console.log("Board nr " + boardIndex + " has lost, last number was " + number)
             console.log("That with a score of " + score)
-
             break
         }
     }
+}
 
-    if(winnerExists)
-        break
+function CountWinningBoards(boards){
+    var count = 0
+    for(var boardIndex in boards){
+        var board = boards[boardIndex]
+
+        if(VerifyWinner(board)){
+            count++
+        }
+    }
+    return count
 }
 
 function VerifyWinner(board){
@@ -115,10 +153,29 @@ function VerifyNumbers(numbers){
     for(var numberIndex in numbers){
         var number = numbers[numberIndex]
 
-        if(number != markedNumber){
+        if(number > 0){
             result = false
             break
         }
     }
     return result
+}
+
+function calculateScore(board, winningNumber){
+    var sumUnmarked = 0
+    var score = 0
+
+    for(var rowIndex in board){
+        var row = board[rowIndex]
+
+        for(var numberIndex in row){
+            var number = row[numberIndex]
+
+            if(number > 0)
+                sumUnmarked += parseInt(number)                    
+        }
+    }
+
+    score = sumUnmarked * parseInt(winningNumber)
+    return score
 }
