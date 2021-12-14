ee329506bcac05de287597e8250504a8af8f5ac1
JefvdA <jef.v.d.a@live.be>
2021-12-14 12:13:17 +0100

    day6 - easier

--- day6.js
+++ day6.js
@@ -5,7 +5,7 @@ var input = fs.readFileSync('./input/day6.txt').toString()
 const DAYS = 256
 const INTERVAL = 7
 
-var fishArray = createFishArray(input )
+var fishArray = createFishArray(input)
 var fishCount = fishArrayToCount(fishArray)
 
 fishCount = progressSimulation(fishCount, DAYS)
@@ -19,59 +19,8 @@ function progressSimulation(fishCount, days){
         simulationCount[day] = count
     }
 
-    var fullCycles = Math.floor(days / INTERVAL)
-    for(var cycle = 0; cycle < fullCycles; cycle++){
-        var currentCount = []
-        for(var day in simulationCount){
-            var count = simulationCount[day]
-            currentCount[day] = count
-        }
-
-        // Fish will produce new fish
-        for(var day = 0; day < INTERVAL; day++){
-            var count = currentCount[day]
-
-            // Current timer stays the same - new fish timer = current + 2
-            var newDay = day + 2
-            simulationCount[newDay] += count
-        }
-        // Fish won't reproduce
-        for(var day = INTERVAL; day < currentCount.length; day++){
-            var count = currentCount[day]
-
-            // Current counter decreases with INTERVAL
-            var newDay = day - INTERVAL
-            simulationCount[newDay] += count
-            simulationCount[day] -= count
-        }
-    }
-
-    var remainingDays = days % INTERVAL
-    var currentCount = []
-    for(var day in simulationCount){
-        var count = simulationCount[day]
-        currentCount[day] = count
-    }
-    // Fish will produce new fish
-    for(var day = 0; day < remainingDays; day++){
-        var count = currentCount[day]
-
-        // Current timer is 6
-        simulationCount[6] += count
-        simulationCount[day] -= count
-
-        // new fish timer is 8
-        var newDay = 8
-        simulationCount[newDay] += count
-    }
-    // Fish won't reproduce
-    for(var day = remainingDays; day < currentCount.length; day++){
-        var count = currentCount[day]
-
-        // Current counter decreases with Remaining days
-        var newDay = day - remainingDays
-        simulationCount[newDay] += count
-        simulationCount[day] -= count
+    for(var day = 0; day < days; day++){
+        simulationCount[(day + 7)%9] += simulationCount[day%9]
     }
 
     return simulationCount
@@ -107,11 +56,4 @@ function countFish(fishArray){
         count += fishArray[fishCount]
 
     return count
-}
-
-function printArray(fishArray){
-    for(var fishIndex in fishArray){
-        var fish = fishArray[fishIndex]
-        console.log(fish)
-    }
 }
