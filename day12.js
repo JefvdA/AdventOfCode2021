6a56e42b5d736e360b9ffcbae7b801c80f656898
Jef <jef.v.d.a@live.be>
2021-12-21 11:49:16 +0100

    Day 12

--- day12.js
+++ day12.js
@@ -1,7 +1,8 @@
 import fs from 'fs'
 
-var input = fs.readFileSync('./input/testing.txt').toString().split("\n")
+var input = fs.readFileSync('./input/day12.txt').toString().split("\n")
 
+// Define class for a point
 class Point {
     constructor(name){
         this.name = name
@@ -11,8 +12,18 @@ class Point {
     addDestination(destination){
         this.destinations.push(destination)
     }
+
+    toString(){
+        var str = "Name : " + this.name + " Destinations: "
+        for(var pointIndex in this.destinations){
+            var point = this.destinations[pointIndex]
+            str += point.name + " / "
+        }
+        return str + "\n"
+    }
 }
 
+// Create array with all the points
 var points = []
 for(var lineIndex in input){
     var line = input[lineIndex].trim()
@@ -36,9 +47,81 @@ for(var lineIndex in input){
 
     // Add "to" as destination to "from"
     fromPoint.addDestination(toPoint)
-    toPoint.addDestination(fromPoint)
+    if(fromPoint.name != "start" && fromPoint.name)
+        toPoint.addDestination(fromPoint)
+}
+var startPoint = points.filter(point => point.name == "start")[0] // Find the starting point
+
+// PART 1
+var pathsV1 = FindPathsV1(startPoint, [], "", []) // Begin of FindPaths, with default values and the startingPoint
+console.log("V1: There are " + pathsV1.length + " possible ways to go through the caves")
+
+// PART 2
+var pathsV2 = FindPathsV2(startPoint, [], false, "", []) // Begin of FindPaths, with default values and the startingPoint
+console.log("V2: There are " + pathsV2.length + " possible ways to go through the caves")
+
+// Recursive function to find the possible paths through the caves - PART 1
+function FindPathsV1(point, visitedSmallCaves, currentPath, paths){
+    currentPath += point.name // Add new point to the path
+
+    // Copy items of visitedSmallCaves so that the values aren't overwritten
+    var smallCaves = []
+    for(var caveIndex in visitedSmallCaves){
+        smallCaves.push(visitedSmallCaves[caveIndex])
+    }
+
+    if(smallCaves.includes(point.name)) // Check if point isn't already been visisted as a small cave
+        return
+    if(point.name == "end") // Check if we reached the end
+        return paths.push(currentPath)
+
+    if(point.name == point.name.toLowerCase())
+        smallCaves.push(point.name)
+
+    for(var destinationIndex in point.destinations){
+        var destination = point.destinations[destinationIndex]
+
+        FindPathsV1(destination, smallCaves, currentPath, paths)
+    }
+    return paths
 }
 
-for(var pointIndex in points){
-    
+// Recursive function to find the possible paths through the caves - PART 2
+function FindPathsV2(point, visitedSmallCaves, specialCaveVisited, currentPath, paths){
+    if(point.name == "start" && currentPath.includes("start"))
+        return
+
+    currentPath += point.name // Add new point to the path
+
+    // Copy items of visitedSmallCaves so that the values aren't overwritten
+    var smallCaves = []
+    for(var caveIndex in visitedSmallCaves){
+        smallCaves.push(visitedSmallCaves[caveIndex])
+    }
+
+    if(smallCaves.includes(point.name)) // Check if point isn't already been visisted as a small cave
+        if(!specialCaveVisited)
+            specialCaveVisited = true
+        else
+            return
+    if(point.name == "end") // Check if this is the end
+        return paths.push(currentPath)
+
+    if(point.name == point.name.toLowerCase())
+        smallCaves.push(point.name)
+
+    for(var destinationIndex in point.destinations){
+        var destination = point.destinations[destinationIndex]
+
+        FindPathsV2(destination, smallCaves, specialCaveVisited, currentPath, paths)
+    }
+    return paths
+}
+
+// A function that prints the points in a more visual way
+function printPoints(points){
+    for(var pointIndex in points){
+        var point = points[pointIndex]
+        console.log(point.toString())
+    }
 }
